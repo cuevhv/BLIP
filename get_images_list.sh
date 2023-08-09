e337da008ffdebffa1a90d51dcded74d42ef10bf
Hanz <cuevhv@yahoo.com>
2023-08-09 10:43:22 +0200

    inputs now are arguments instead of being hardcoded inside the script

--- get_images_list.sh
+++ get_images_list.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# ./get_images_list.sh dataset/evermotion_dataset out_list.txt
 
 # Check if the number of arguments is correct
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 <folder_path> <output_file>"
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <folder_path> <searching_name> <extension>"
   exit 1
 fi
 
 # Get the folder path and output file from the command line arguments
 folder_path="$1"
-output_file="$2"
+searching_name="$2"  # tonemap
+extension="$3"  #.jpg
 
 original_folder_path="$folder_path"
 folder_path=$(realpath "$folder_path")
@@ -18,24 +18,28 @@ folder_path=$(realpath "$folder_path")
 # Pipe the results to 'grep' to filter files containing 'color' in their names.
 # The results will be stored in the 'color_files' array.
 color_files=()
+
+echo "starting search"
 while IFS= read -r -d '' file; do
-  if echo "$file" | grep -iq "color"; then
+  echo $file
+  if echo "$file" | grep -iq "${searching_name}"; then
     color_files+=("$original_folder_path/$file")
+    echo $file
   fi
 # done < <(find "$folder_path" -type f -print0)
-done < <(find "$folder_path" -type f -iname "*color.jpg*" -printf '%P\0')
+done < <(find "$folder_path" -type f -iname "*${searching_name}*.${extension}" -printf '%P\0')
 
 # Sort the list of files in alphabetical order
 sorted_color_files=($(printf '%s\n' "${color_files[@]}" | sort))
 
 
 # Output the list of files found and save them to the output text file.
-save_pth="$original_folder_path/$output_file"
+save_pth="$original_folder_path/dataset_imgs_list.out"
 if [ ${#sorted_color_files[@]} -gt 0 ]; then
   echo "Files with 'color' in the name:"
   printf '%s\n' "${sorted_color_files[@]}"
   printf '%s\n' "${sorted_color_files[@]}" > "$save_pth"
   echo "Paths saved to $save_pth"
 else
-  echo "No files with 'color' in the name found."
+  echo "No files with '${searching_name}' in the name found."
 fi
