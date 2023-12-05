33a7e2d48a11a16be03be3219023e79e53a3e229
Hanz <cuevhv@yahoo.com>
2023-12-05 11:05:55 +0100

    working example for labelling

--- text_captioning.py
+++ text_captioning.py
@@ -2,6 +2,9 @@ import torch
 import requests
 from PIL import Image
 from transformers import AutoModelForCausalLM, LlamaTokenizer
+import warnings
+warnings.filterwarnings('ignore')
+
 
 tokenizer = LlamaTokenizer.from_pretrained('lmsys/vicuna-7b-v1.5')
 model = AutoModelForCausalLM.from_pretrained(
@@ -14,8 +17,12 @@ model = AutoModelForCausalLM.from_pretrained(
 
 # chat example
 
-query = "Describe the people in the scene, what they are wearing. If there is a green screen, don't describe the green screen."
+query = ("Describe the person in the scene, what the person is wearing. "
+        "If there is a green screen, don't describe the green screen."
+        "If there is a head band, don't descbre the head band.")
+print(query)
 image = Image.open("/home/hcuevas/Desktop/01186.jpg").convert('RGB')
+image.save("tmp.png")
 inputs = model.build_conversation_input_ids(tokenizer, query=query, history=[], images=[image])  # chat mode
 inputs = {
     'input_ids': inputs['input_ids'].unsqueeze(0).to('cuda'),
@@ -23,9 +30,12 @@ inputs = {
     'attention_mask': inputs['attention_mask'].unsqueeze(0).to('cuda'),
     'images': [[inputs['images'][0].to('cuda').to(torch.bfloat16)]],
 }
-gen_kwargs = {"max_length": 2048, "do_sample": False}
+gen_kwargs = {"max_length": 2048, "do_sample": True,
+              "top_p": 0.4, "top_k": 5, "temperature": 0.8,
+            #   "num_beams": 10, "early_stopping": True,
+              }
 
 with torch.no_grad():
     outputs = model.generate(**inputs, **gen_kwargs)
     outputs = outputs[:, inputs['input_ids'].shape[1]:]
-    print(tokenizer.decode(outputs[0]))
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
